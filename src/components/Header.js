2805362c842b00fee5f273f476498e680d218c38
Keturah Howard <keturah.dev@gmail.com>
2020-05-15 09:32:52 -0700

    change component colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return(
-    <div style={{border: "2px solid yellow"}}>
+    <div style={{border: "2px solid aqua"}}>
       <h1>Raiser</h1>
     </div>
   );
--- src/components/JoinForm.js
+++ src/components/JoinForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function JoinForm() {
   return(
-    <div style={{border: "2px solid aqua"}} >
+    <div style={{border: "2px solid orange"}} >
       <p>This will hold a form that takes in the room pin, and will then display/export to type-of-member form</p>
     </div>
   );
