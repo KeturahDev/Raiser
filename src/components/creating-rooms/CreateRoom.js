2c4588337e2cf838fa38045771ef559f0d8af98f
Keturah Howard <keturah.dev@gmail.com>
2020-05-15 11:57:56 -0700

    add toggle functionality between form and confirm

--- src/components/creating-rooms/CreateRoom.js
+++ src/components/creating-rooms/CreateRoom.js
@@ -5,22 +5,33 @@ import Confirmation from './Confirmation';
 
 function CreateRoom(){
   const [number, changeNumber] = useState(Math.floor(10000 + Math.random() * 90000))
-  
+  const [visChildCR, changeVisChildCR] = useState("form")
+
   function getNumber(){
     const newNum = Math.floor(10000 + Math.random() * 90000)
     changeNumber(newNum)
   }
+  
   const makeRoom = (password) => {
     const newRoom = { roomPin: number, teacherPassword: password }
     console.log("Room to be saved: ", newRoom)
-  } 
+    changeVisChildCR("confirm")
+  }
 
   return(
     <div style={{border: "2px solid purple"}}>
       <h2>Adding a Room!</h2>
-      <GeneratePin pin={number} generatePin={getNumber}/>
-      <Form onSubmitPassword={makeRoom} />
-      <Confirmation pin={number}/>
+      {
+        visChildCR === "form" &&
+        <div>
+          <GeneratePin pin={number} generatePin={getNumber}/>
+          <Form onSubmitPassword={makeRoom} /> 
+        </div>
+      }
+      {
+        visChildCR === "confirm" &&
+        <Confirmation pin={number}/>
+      }
     </div>
   )
 }
