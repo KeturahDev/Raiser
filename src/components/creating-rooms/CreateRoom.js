32597f34c29532a70855a85131d80471d5a6eac5
Keturah Howard <keturah.dev@gmail.com>
2020-05-15 11:32:23 -0700

    create password form and function that will submit the room to db

--- src/components/creating-rooms/CreateRoom.js
+++ src/components/creating-rooms/CreateRoom.js
@@ -10,11 +10,16 @@ function CreateRoom(){
     const newNum = Math.floor(10000 + Math.random() * 90000)
     changeNumber(newNum)
   }
+  const makeRoom = (password) => {
+    const newRoom = { roomPin: number, teacherPassword: password }
+    console.log("Room to be saved: ", newRoom)
+  } 
+
   return(
     <div style={{border: "2px solid purple"}}>
       <h2>Adding a Room!</h2>
       <GeneratePin pin={number} generatePin={getNumber}/>
-      <Form />
+      <Form onSubmitPassword={makeRoom} />
       <Confirmation pin={number}/>
     </div>
   )
--- src/components/creating-rooms/Form.js
+++ src/components/creating-rooms/Form.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Form(){
+function Form(props){
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    if(event.target.pwd.value !== event.target.pwd2.value){
+      alert("Passwords do not match")
+    } else {
+      props.onSubmitPassword(event.target.pwd.value)
+    }
+  }
   return(
     <div style={{border: "2px solid green"}}>
-      <p>form with teacher password</p>
-      <p> stretch.. input that must be the same as the input above</p>
+      <form onSubmit={handleSubmitForm}>
+        <input type="password" placeholder="Enter Password" name="pwd" required/>
+        <input type="password" placeholder="Confirm Password" name="pwd2" required/>
+        <button type="submit">submit</button>
+      </form>
     </div>
   )
 }
-
+Form.propTypes = {
+  // pin: PropTypes.number
+  onSubmitPassword: PropTypes.func
+}
 export default Form;
