91114fea41669b43749cc56e7c0e8b751862058a
Keturah Howard <keturah.dev@gmail.com>
2020-05-15 09:31:43 -0700

    add state to splash to toggle between joining and creating a new room

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import Header from './Header'
-import JoinForm from './JoinForm'
+import React, {useState} from 'react';
+import Header from './Header';
+import JoinForm from './JoinForm';
+import CreateRoom from './CreateRoom'
 
 function Splash() {
+  const [visibleChild, changeVisChild] = useState("join")
+
+  const handleClick = () => {
+    if(visibleChild === "join") {
+      changeVisChild("new")
+    } else if (visibleChild === "new") {
+      changeVisChild("join")
+    }
+    console.log("toggled to: ", visibleChild)
+  }
+  const renderingChild = () => {
+    if(visibleChild === "join") {
+      return {
+        component: <JoinForm />,
+        buttontext: "Create Room"
+      } 
+    } else {
+      return {
+        component: <CreateRoom />,
+        buttontext: "Join Room"
+      }
+    }
+  }
+  const renderedChild = renderingChild()
   return(
     <div style={{border: "2px solid blue"}}>
       <p>this is splash coponent</p>
       <Header />
-      <JoinForm />
-      <button>Create New Room</button>
+      {renderedChild.component}
+      <button onClick={handleClick}>{renderedChild.buttontext}</button>
     </div>
   );
 }
