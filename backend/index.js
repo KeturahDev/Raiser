d28a9e826e4ee773624ba5228fa387f7b1af7ff4
Keturah Howard <keturah.dev@gmail.com>
2020-05-18 16:05:49 -0700

    export models, create first endpoint with express

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 require('./db/mongoose')
+const Room = require('./models/room')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/rooms', (req,res) => {
+  const room = new Room(req.body)
+  room.save().then(() => {
+    res.send(room)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
+
 
 app.listen(port, () => {
   console.log('Server is up and ruinning ' + port)
