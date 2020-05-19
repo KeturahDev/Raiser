79ae289da79dd7cabfd011356c12adfe93e31d3d
Keturah Howard <keturah.dev@gmail.com>
2020-05-19 16:41:58 -0700

    create component where room pin will be entered to enter state

--- frontend/src/components/JoinView.js
+++ frontend/src/components/JoinView.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
-import EnterRoom from './forms/EnterRoom'
+import EnterRoom from './forms/EnterRoom';
+import ChooseRoom from './forms/ChooseRoom';
 import TypeOfUserForm from './forms/TypeOfUserForm';
 
 function JoinView() {
   
   const gatherFormInputs = (inputs) => {
-    if (inputs.type === "student") {
-      changeCurrentComponent(<EnterRoom studentName={inputs.studentName}/>)
-    } else {
-      changeCurrentComponent(<EnterRoom teacherPassword={inputs.teacherPassword}/>)
-    }
+    if(inputs) {
+      if (inputs.type === "student") {
+        changeCurrentComponent(<EnterRoom studentName={inputs.studentName}/>)
+      } else {
+        changeCurrentComponent(<EnterRoom teacherPassword={inputs.teacherPassword}/>)
+      }
+    } 
+    // else {
+    //   changeCurrentComponent(<TypeOfUserForm gatherFormInputs={gatherFormInputs} />)
+    // }
   }
 
-  const [currentComponent, changeCurrentComponent] = useState(<TypeOfUserForm gatherFormInputs={gatherFormInputs} />)
+  const RoomEntersState = (roomPin) => {
+    console.log("room to be sent to GET api call: ", roomPin)
+    changeCurrentComponent(<TypeOfUserForm gatherFormInputs={gatherFormInputs} />)
+  }
+
+  const [currentComponent, changeCurrentComponent] = useState(<ChooseRoom RoomEntersState={RoomEntersState} />)
 
   return (
     <div style={{ border: "2px solid orange" }} >
---
+++ frontend/src/components/forms/ChooseRoom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChooseRoom(){
+  return(
+    <div style={{border: "2px solid darkred"}}>
+      <p>Where you enter pin, and room enters state</p>
+    </div>
+  )
+}
+
+export default ChooseRoom
