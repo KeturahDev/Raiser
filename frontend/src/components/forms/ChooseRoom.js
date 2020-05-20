78af532bda4858133ff605322cadcce16cd489f0
Keturah Howard <keturah.dev@gmail.com>
2020-05-20 08:38:31 -0700

    Run into issue: cannot send req with bodies with GET calls.

--- frontend/src/components/forms/ChooseRoom.js
+++ frontend/src/components/forms/ChooseRoom.js
@@ -2,29 +2,29 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 function ChooseRoom(props){
-  // const [summonedRoom, pullRoom] = useState({})
+  const [summonedRoom, pullRoom] = useState({})
 
-  // const getRoom = (roomPin) => {
-  //   console.log("ROOM PIN: ",roomPin)
-  //   fetch('http://127.0.0.1:5000/room',
-  //   {method: "GET",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   },
-  //   body: JSON.stringify(roomPin)})
-  //   .then((res) => res.json())
-  //   .then(jsonifiedRes => {
-  //     console.log(jsonifiedRes)
-  //     pullRoom(jsonifiedRes)
-  //   }).catch(error => console.log("Unable to fertch room:  "error))
-  // }
+  const getRoom = (roomPin) => {
+    console.log("ROOM PIN: ",roomPin)
+    fetch('http://127.0.0.1:5000/room',
+    {method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(roomPin)})
+    .then((res) => res.json())
+    .then(jsonifiedRes => {
+      console.log("GOT IT:  ", jsonifiedRes)
+      pullRoom(jsonifiedRes)
+    }).catch(error => console.log("Unable to fetch room:  ", error))
+  }
 
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const pinObj = { pin: event.target.pin.value }
     console.log("HANDLE FUNC PIN OBJ:  ", pinObj)
-    // getRoom(event.target.pin)
+    getRoom(pinObj)
   }
   return(
     <div style={{border: "2px solid darkred"}}>
