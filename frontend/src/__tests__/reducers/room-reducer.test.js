13623f8c24658d8bb39cca80b1dc4cbe5e0d5e7c
Keturah Howard <keturah.dev@gmail.com>
2020-05-20 09:40:40 -0700

    recieve a meaningful fail for second test for room reducer

--- frontend/src/__tests__/reducers/room-reducer.test.js
+++ frontend/src/__tests__/reducers/room-reducer.test.js
@@ -1,7 +1,16 @@
 import roomReducer from './../../reducers/room-reducer'
 
 describe('roomReducer', () => {
+  let action;
+  const room = {pin: 11223344, teacherPassword: "blackcat", id:1234}
   test('should return default state of reducer', () => {
     expect(roomReducer({}, {type:null})).toEqual({})
+  })
+  test('should make room the reducers state', () => {
+    action = {
+      action: 'ENTER_ROOM',
+      room //hopefully accesses outter const to do shorthand 
+    }
+    expect(roomReducer({}, action)).toEqual({pin: 11223344, teacherPassword: "blackcat", id:1234})
   })
 })
