7bd11921172938906a4a32275f256181c4f8f600
Keturah Howard <keturah.dev@gmail.com>
2020-05-20 10:02:08 -0700

    create redux store with reducer and pass it into application's scope

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import roomReducer from './reducers/room-reducer'
+
+const store = createStore(roomReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
